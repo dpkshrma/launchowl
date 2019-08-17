71880dc46adbae62ecc07803da8b1c179d41db5a
Deepak <deepak.sharma@goniyo.com>
2019-08-17 09:29:15 +0530

    Updates

---
+++ precache-manifest.2b333845cc0fc07ce9287ab14bf1ce44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1471f1f96d01a7b62e1338457154d5b3",
+    "url": "/launchowl/index.html"
+  },
+  {
+    "revision": "7589f49493d77a0d1319",
+    "url": "/launchowl/static/css/main.41a1232f.chunk.css"
+  },
+  {
+    "revision": "f0bbc3b37e6017313d4b",
+    "url": "/launchowl/static/js/2.d95b16c8.chunk.js"
+  },
+  {
+    "revision": "7589f49493d77a0d1319",
+    "url": "/launchowl/static/js/main.83821084.chunk.js"
+  },
+  {
+    "revision": "bf495f8b8dfdf5eb0a62",
+    "url": "/launchowl/static/js/runtime~main.7c018b2a.js"
+  },
+  {
+    "revision": "bf911752163450a8f48757e62cf760f5",
+    "url": "/launchowl/static/media/owl.bf911752.svg"
+  }
+]);
